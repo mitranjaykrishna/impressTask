5b256d247c61e4fc70a1caea2755baaf15dad179
Mitranjay Krishna <mitranjaykrishna@gmail.com>
2023-04-04 18:55:49 +0530

    namee email address phone added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <FloatingLabel controlId="floatingText" label="Name" className="mb-3">
+      <Form.Control type="text" placeholder="Name" />
+      </FloatingLabel>
+    
+      <FloatingLabel controlId="floatingInput" label="Email" className="mb-3">
+      <Form.Control type="email" placeholder="name@example.com" />
+      </FloatingLabel>
+
+      <FloatingLabel controlId="floatingPhone" label="Phone" className="mb-3">
+      <Form.Control type="text" placeholder="Phone" />
+      </FloatingLabel>
+
+      <FloatingLabel controlId="floatingTextarea" label="Address" className="mb-3">
+        <Form.Control as="textarea" placeholder="Leave a Address here" />
+      </FloatingLabel>
     </div>
+
+    </>
   );
 }
 
